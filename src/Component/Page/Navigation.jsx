6f0a52d566d4fbe1ee49308a31a6b1f5921dced6
fauzipradipta <dipta0988@gmail.com>
2023-02-20 16:38:48 -0800

    update naviation.jsx

--- src/Component/Page/Navigation.jsx
+++ src/Component/Page/Navigation.jsx
@@ -19,13 +19,15 @@ export default function Navigation() {
       </div>
       <div className="Rightside">
         <Link to="/home"> Home </Link>
-        <Link to="/menu"> Menu </Link>        
+        <Link to="/menu"> Menu </Link>       
+        
         {/* <Link to="/contact"> Contact </Link> */}
         {/* <Link to="/cart">Cart</Link> */}
       </div>
       <Routes>
         <Route path="/home" exact element={<Home />} />
         <Route path="/menu" element={<Menu />}></Route>        
+         
         {/* <Route path="/contact" element={<Contact />} /> */}
         {/* <Route path="/cart" element={<Cart />} /> */}
         <Route path="/" element={<Home />} />
